8ca79277313b72baaa4990bd990b6aaebaa4d97a
gabrielPistore <devgbpistore@gmail.com>
2024-06-30 22:43:28 -0300

    update Employee and Category models

--- core/models.py
+++ core/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 
 class Employee(models.Model):
-    pass
+    name: models.CharField = models.CharField(max_length=150)
+    email: models.EmailField = models.EmailField()
+    phone: models.CharField = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    pass
+    name: models.CharField = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
 
 class WorkOrder(models.Model):
     pass
