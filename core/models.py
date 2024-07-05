3e2d8866ec549b156f6e7a2f66c8f7c329fc3b33
gabrielPistore <devgbpistore@gmail.com>
2024-07-05 14:21:18 -0300

    change fields max length

--- core/models.py
+++ core/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name: models.CharField = models.CharField(max_length=150)
+    name: models.CharField = models.CharField(max_length=255)
     email: models.EmailField = models.EmailField()
-    phone: models.CharField = models.CharField(max_length=11)
+    phone: models.CharField = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name: models.CharField = models.CharField(max_length=150)
+    name: models.CharField = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 class WorkOrder(models.Model):
     pass
