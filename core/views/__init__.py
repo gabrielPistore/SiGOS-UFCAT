cf2aa4a9b85a560bab1ed2908d66dc846bcfec6e
gabrielPistore <devgbpistore@gmail.com>
2024-07-19 13:51:32 -0300

    rename work_order_form.py to work_order.py

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,4 +1,4 @@
 from .history import History
 from .home import Home
 from .order_management import OrderManagement
-from .work_order_form import WorkOrderFormView
+from .work_order import WorkOrderFormView
